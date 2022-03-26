f535324fb3284cd75c6ce3b7e795c5af1193ce5e
Ivan Tamoshkin <57822372+TamoshkinIvan@users.noreply.github.com>
2022-03-26 20:44:31 +0700

    Автоматическое определение окончания возмущения
    Скрипт для обработки данных с опытами АПВ и УРОВ

--- dataset_making.py
+++ dataset_making.py
@@ -3,210 +3,148 @@ import pandas as pd
 import math
 import numpy as np
 
-
-csv_names = os.listdir(".")
-csv_names.remove('.ipynb_checkpoints')
-csv_names.remove('dataset_making.py')
-csv_names.remove('data_proc_before_kz.ipynb')
-csv_names.remove('data_proc_after_kz.ipynb')
-csv_names.remove('new_res')
-csv_names.remove('test_osib1')
-csv_names.remove('Данные для тестовой схемы')
-csv_names.remove('predict.png')
+csv_names = os.listdir("new_res")
 
 
-
-def get_df_tran(csv_names):
-    it = 0
+def get_df_tran(csv_names, window_size):
     result = pd.DataFrame()
-    while it <= 135:
-        if it in range(0,len(csv_names),4):
-            result = pd.concat([result, pre_processing_apv_data(csv_name=csv_names,i=it)], ignore_index=True)
-        elif it in range(1,len(csv_names),4):
-            result = pd.concat([result, pre_processing_apv250_data(csv_name=csv_names,i=it)], ignore_index=True)
-        elif it in range(2, len(csv_names), 4):
-            result = pd.concat([result, pre_processing_kz250_data(csv_name=csv_names,i=it)], ignore_index=True)
-        elif it in range(3, len(csv_names), 4):
-            result = pd.concat([result, pre_processing_kz300_data(csv_name=csv_names,i=it)], ignore_index=True)
-        it += 1
-    return result
+    time_fall_list = []
+    is_fall_list = []
+    contingency_1 = []
+    full_fall_list = []
+    full_time = []
+    for i in range(0, len(csv_names)):
+        dict_result_1 = pre_processing(csv_name=csv_names[i], window_size=window_size)
+        result = pd.concat([result,
+                            dict_result_1['res']],
+                            ignore_index=True)
+        time_fall_list.append(dict_result_1['time'])
+        is_fall_list.append(dict_result_1['is_fall'])
+        contingency_1.append(dict_result_1['cont'])
+        full_time.append(dict_result_1['time_full'])
+        full_fall_list.append(dict_result_1['full_fall'])
+        i += 1
+    total_result = {
+                    'data': result,
+                    'time_lst': time_fall_list,
+                    'is_fall': is_fall_list,
+                    'cont1': contingency_1,
+                    'full_fall_list': full_fall_list,
+                    'full_time': full_time
+                    }
+
+    time = pd.DataFrame(total_result['time_lst'])
+    time_full = pd.DataFrame(total_result['full_time'])
+    print(total_result['data'].info())
+    print(f"Количество случаев превышения угла 180 градусов: {sum(total_result['is_fall'])}")
+    print(f"Общее количество рассмотренных возмущений {sum(total_result['cont1'])}")
+    print(f"Отношение процент случаев нарушения устойчивости { sum(total_result['is_fall']) / sum(total_result['cont1']) * 100 }")
+    print(time_full.describe())
+    print(time.describe())
+    return total_result
 
 
-#transient_111 = pd.read_csv("test_trans.csv", delimiter = ";",  decimal=".")
-def pre_processing_apv_data(csv_name, i):
+
+def pre_processing(csv_name, window_size):
+    #Данные, которые нам нужны для исследования
+    #Размер входного окна
+    windows_size_ref = int(window_size / (10))
+    #Величина на которую отступаем влево, относительно окончания возмущения
+    left_border = windows_size_ref + 3 + 10
+    # Величина на которую отступаем влево, относительно окончания возмущения
+    right_border = windows_size_ref + 2 + 10
+    #Ведем подсчет сколько по итогу возмущений использовалось для обучения и тестирования
+    contingency = 0
+    #Итоговый датафрейм
     result = pd.DataFrame()
-    w = 0
-    delta_1 = 0
-    time_1 = 0
-    transient = pd.DataFrame()
-    time_2 = 0
-    transient_56 = pd.DataFrame()
+    #Определяем было ли нарушение устойчивости
+    is_stability_fall = 0
+    #Определяем время нарушения устойчивости
+    stabilit_fall_time = None
+    #Определяем время нарушения устойчивости полный проворот
+    full_stabilit_fall_time = None
+    #Полный проворот
+    full_fall = 0
+    #Возвращаемый словарь
+    dict_result =  {
+                    'res': result,
+                    'is_fall': is_stability_fall,
+                    'time': stabilit_fall_time,
+                    'cont': contingency,
+                    'time_full': full_stabilit_fall_time,
+                    'full_fall': full_fall
+                    }
+    #Список с точками плюс минус ноль
+    trans_list = []
+    # Преобразуем полученные данные в датафрейм
+    transient_init = pd.read_csv(f'new_res/{csv_name}',
+                            delimiter=";",
+                            decimal=",",
+                            index_col=False,
+                            on_bad_lines='warn')
+    #Ищем индекс, который от которого будем вести отсчет, обозначет окончание возмущения
+    for index, row in transient_init.iterrows():
+        if index == 0:
+            continue
+        if transient_init['t'].loc[index] - transient_init['t'].loc[index - 1] < 0.001:
+            index_start = index
+            trans_list.append(index_start)
 
     # Считаем необходимые данные
-    transient = pd.read_csv(csv_name[i], delimiter=";",
-                                decimal=",", index_col=False)[71:94:]
-    transient_56 = pd.read_csv(csv_name[i], delimiter=";",
-                                   decimal=",", index_col=False)[68:94:]
+    transient = transient_init[index_start - left_border : index_start + right_border:]
+    transient_56 = transient_init[index_start - left_border - 3 :index_start + right_border:]
 
-    # Избавляемся от выбросов по углу (еще не точно)
-    if (transient['delta_horon'] > 400).any():
-        return result
+    # Избавляемся от плюс минус нуля
+    transient = transient.drop(index_start)
+    transient_56 = transient_56.drop(index_start)
 
-        # Выполним расчеты относительно березовской грэс
-    transient['delta'] = transient['delta_horon']  # - transient['delta_beraza']
-    transient_56['delta'] = transient_56['delta_horon']  # - transient_56['delta_beraza']
+    # Задаем новые индексы
+    transient = transient.reset_index(drop=True)
+    transient_56 = transient_56.reset_index(drop=True)
 
-        # Переводим данные в в формат numpy
-    delta_1 = transient_56["delta"].to_numpy()
-    time_1 = transient_56["t"].to_numpy()
+    # Выполним расчеты относительно березовской грэс
+    transient.loc[:,'delta'] =  transient['delta_horon'] #- transient['delta_vartovsk']
+    transient_56.loc[:, 'delta'] = transient_56['delta_horon'] #- transient_56['delta_vartovsk']
 
-        # Избавляемся от плюс минус нуля
-    transient = transient.drop(index=[82])
+    # Переводим данные в в формат numpy
+    time_1 = transient_56['t'].to_numpy()
+    delta_1 = transient_56['delta'].to_numpy()
 
-        # Теперь удалим точки появляющиеся при расчете ПП плюс минус ноль
-    time_2 = np.delete(time_1, 13)
-    delta = np.delete(delta_1, 13)
+    # Считаем производную
+    w = np.diff(delta_1) / (np.diff(time_1) * 1000)
 
-        # Считаем производную
-    w = np.diff(delta) / (np.diff(time_2) * 1000)
-
-        # Удаляем первую строчку, для соблюденяи размерности
+    # Удаляем первую строчку, для соблюденяи размерности
     w_1 = np.delete(w, 0)
     w_1 = np.delete(w_1, 0)
     transient["w"] = w_1
+
+    # Определяем время нарушения устойчивости и факт нарушения устойчивости
+    for index, row in transient.iterrows():
+        if row['delta'] > 180:
+            is_stability_fall = 1
+            stabilit_fall_time = (row['t'] - transient_init['t'].loc[index_start]) * 1000
+            break
+
+    # Определяем время проворота на 360
+    for index, row in transient.iterrows():
+        if row['delta'] > 250:
+            full_fall = 1
+            full_stabilit_fall_time = (row['t'] - transient_init['t'].loc[index_start]) * 1000
+            break
+
+    # Возращаем единичку, т.к. у нас это возмущение идет в обущающую выборку
+    contingency += 1
+
+    #Получаем итоговый датафрейм
     result = pd.concat([result, transient], ignore_index=True)
-    i += 4
-    return result
+    dict_result =  {
+                    'res': result,
+                    'is_fall': is_stability_fall,
+                    'time': stabilit_fall_time,
+                    'cont': contingency,
+                    'time_full': full_stabilit_fall_time,
+                    'full_fall': full_fall
+                    }
+    return dict_result
 
-
-def pre_processing_apv250_data(csv_name, i):
-    result = pd.DataFrame()
-    w = 0
-    delta_1 = 0
-    time_1 = 0
-    transient = pd.DataFrame()
-    time_2 = 0
-    transient_56 = pd.DataFrame()
-
-    # Считаем необходимые данные
-    transient = pd.read_csv(csv_name[i], delimiter=";",
-                                decimal=",", index_col=False)[66:89:]
-    transient_56 = pd.read_csv(csv_name[i], delimiter=";",
-                                   decimal=",", index_col=False)[63:89:]
-
-    # Избавляемся от выбросов по углу (еще не точно)
-    if (transient['delta_horon'] > 400).any():
-            return result
-
-
-        # Выполним расчеты относительно березовской грэс
-    transient['delta'] = transient['delta_horon']  # - transient['delta_beraza']
-    transient_56['delta'] = transient_56['delta_horon']  # - transient_56['delta_beraza']
-
-        # Переводим данные в в формат numpy
-    delta_1 = transient_56["delta"].to_numpy()
-    time_1 = transient_56["t"].to_numpy()
-
-        # Избавляемся от плюс минус нуля
-    transient = transient.drop(index=[77])
-        # Теперь удалим точки появляющиеся при расчете ПП плюс минус ноль
-    time_2 = np.delete(time_1, 13)
-    delta = np.delete(delta_1, 13)
-
-        # Считаем производную
-    w = np.diff(delta) / (np.diff(time_2) * 1000)
-
-        # Удаляем первую строчку, для соблюденяи размерности
-    w_1 = np.delete(w, 0)
-    w_1 = np.delete(w_1, 0)
-    transient["w"] = w_1
-    result = pd.concat([result, transient], ignore_index=True)
-    i += 4
-    return result
-
-
-def pre_processing_kz250_data(csv_name, i):
-        result = pd.DataFrame()
-        w = 0
-        delta_1 = 0
-        time_1 = 0
-        transient = pd.DataFrame()
-        time_2 = 0
-        transient_56 = pd.DataFrame()
-
-        # Считаем необходимые данные
-        transient = pd.read_csv(csv_name[i], delimiter=";",
-                                decimal=",", index_col=False)[29:52:]
-        transient_56 = pd.read_csv(csv_name[i], delimiter=";",
-                                   decimal=",", index_col=False)[26:52:]
-        # Избавляемся от выбросов по углу (еще не точно)
-        if (transient['delta_horon'] > 400).any():
-            return result
-
-        # Выполним расчеты относительно березовской грэс
-        transient['delta'] = transient['delta_horon']  # - transient['delta_beraza']
-        transient_56['delta'] = transient_56['delta_horon']  # - transient_56['delta_beraza']
-
-        # Переводим данные в в формат numpy
-        delta_1 = transient_56["delta"].to_numpy()
-        time_1 = transient_56["t"].to_numpy()
-
-        # Избавляемся от плюс минус нуля
-        transient = transient.drop(index=[40])
-
-        # Теперь удалим точки появляющиеся при расчете ПП плюс минус ноль
-        time_2 = np.delete(time_1, 13)
-        delta = np.delete(delta_1, 13)
-
-        # Считаем производную
-        w = np.diff(delta) / (np.diff(time_2) * 1000)
-
-        # Удаляем первую строчку, для соблюденяи размерности
-        w_1 = np.delete(w, 0)
-        w_1 = np.delete(w_1, 0)
-        transient["w"] = w_1
-        result = pd.concat([result, transient], ignore_index=True)
-        i += 4
-        return result
-
-def pre_processing_kz300_data(csv_name, i):
-        result = pd.DataFrame()
-        w = 0
-        delta_1 = 0
-        time_1 = 0
-        transient = pd.DataFrame()
-        time_2 = 0
-        transient_56 = pd.DataFrame()
-        #Считаем необходимые данные
-        transient = pd.read_csv(csv_name[i], delimiter=";",
-                                decimal=",", index_col=False)[31:54:]
-        transient_56 = pd.read_csv(csv_name[i], delimiter=";",
-                                   decimal=",", index_col=False)[28:54:]
-        #Избавляемся от выбросов по углу (еще не точно)
-        if (transient['delta_horon'] > 400).any():
-            return result
-        #Выполним расчеты относительно березовской грэс
-        transient['delta'] = transient['delta_horon'] #- transient['delta_beraza']
-        transient_56['delta'] = transient_56['delta_horon'] #- transient_56['delta_beraza']
-        #Переводим данные в в формат numpy
-        delta_1 = transient_56["delta"].to_numpy()
-        time_1 = transient_56["t"].to_numpy()
-
-        #Избавляемся от плюс минус нуля
-        transient = transient.drop(index=[42])
-        #Теперь удалим точки появляющиеся при расчете ПП плюс минус ноль
-        time_2 = np.delete(time_1, 14)
-        delta = np.delete(delta_1, 14)
-
-        #Считаем производную
-        w = np.diff(delta) / (np.diff(time_2) * 1000)
-
-        #Удаляем первую строчку, для соблюденяи размерности
-        w_1 = np.delete(w, 0)
-        w_1 = np.delete(w_1, 0)
-        transient["w"] = w_1
-        result = pd.concat([result, transient], ignore_index=True)
-        return result
-
-
-df = get_df_tran(csv_names)
+df = get_df_tran(csv_names, window_size=200)
